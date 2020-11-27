e9025803ce0088d4aa2d8f4661f5ac28967ed78d
大石一八 <i1no1su6ke@gmail.com>
2020-11-27 21:02:37 +0900

    voice_actorsテーブル作成

---
+++ app/models/voice_actor.rb
@@ -0,0 +1,2 @@
+class VoiceActor < ApplicationRecord
+end
---
+++ db/migrate/20201127115216_create_voice_actors.rb
@@ -0,0 +1,19 @@
+class CreateVoiceActors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :voice_actors do |t|
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :prefecture, null: false
+      t.string :address, null: false, default: ''
+      t.string :wikipedia_url, null: false, default: ''
+      t.string :twitter_url, null: false, default: ''
+
+      t.timestamps
+
+      t.index :last_name_kana
+      t.index :prefecture
+    end
+  end
+end
