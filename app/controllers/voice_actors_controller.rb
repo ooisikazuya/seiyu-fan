1194759cf3dd9d980c35d5e4ba7b87ffd5dba07d
大石一八 <i1no1su6ke@gmail.com>
2020-12-04 21:21:27 +0900

    kana_low

--- app/controllers/voice_actors_controller.rb
+++ app/controllers/voice_actors_controller.rb
@@ -1,5 +1,10 @@
 class VoiceActorsController < ApplicationController
   def index
-    @voice_actors =  VoiceActor.all
+    kana_row = params[:kana_row] || 'あ'
+    @voice_actors = if kana_row == 'あ'
+                       VoiceActor.where('last_name_kana like ? or last_name_kana like ? or last_name_kana like ? or last_name_kana like ? or last_name_kana like ?', "あ%", "い%", "う%", "え%", "お%")
+                     elsif kana_row == 'か'
+                       VoiceActor.where('last_name_kana like ? or last_name_kana like ? or last_name_kana like ? or last_name_kana like ? or last_name_kana like ?', "か%", "き%", "く%", "け%", "こ%")
+                     end
   end
 end
