7f4a7fa300dce7657b1b380db5150fced876248f
大石一八 <i1no1su6ke@gmail.com>
2020-12-11 20:38:43 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :logins, only: [:new, :create, :destroy]
   resources :users, only: [:index, :show, :new, :create, :edit, :update]
   resources :posts, only: [:index, :show, :new, :create] 
   resources :hometowns, only: [:index, :show]
   resources :voice_actors, only: [:index]
-  resources :top, only: [:index]
+  root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_070221) do
+ActiveRecord::Schema.define(version: 2020_12_11_112534) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   create_table "voice_actors", force: :cascade do |t|
     t.string "last_name", null: false
-    t.string "first_name", default: "", null: false
+    t.string "first_name", null: false
     t.string "last_name_kana", null: false
-    t.string "first_name_kana", default: "", null: false
+    t.string "first_name_kana", null: false
     t.string "prefecture", null: false
     t.string "address", default: "", null: false
     t.string "wikipedia_url", default: "", null: false
@@ -28,4 +45,3 @@ ActiveRecord::Schema.define(version: 2020_12_10_070221) do
   end
 
 end
-
