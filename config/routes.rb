6409f508ada4f245df5b5c06b351bf3ae6059a8a
大石一八 <i1no1su6ke@gmail.com>
2020-12-12 10:04:18 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :new, :create, :edit, :update]
   resources :posts, only: [:index, :show, :new, :create] 
   resources :hometowns, only: [:index, :show]
-  resources :voice_actors, only: [:index]
+  resources :voice_actors, only: [:index] do
+    resources :users
+  end
   root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
