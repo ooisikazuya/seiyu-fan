b4c7c2c0dc7fa888a354d685e836cc7e365c4b58
ooisikazuya <i1no1su6ke@gmail.com>
2020-12-21 14:49:23 +0900

    デフォルト値の設定

--- app/controllers/favorite_voice_actors_controller.rb
+++ app/controllers/favorite_voice_actors_controller.rb
@@ -1,12 +1,15 @@
 class FavoriteVoiceActorsController < ApplicationController
   def edit
     @voice_actors_array = VoiceActor.all.map { |actor| ["#{actor.last_name} #{actor.first_name}", actor.id] }
+    @user = current_user
+    favorite_voice_actor_ids = @user.favorite_voice_actors.pluck(:voice_actor_id)
+    @favorite_voice_actors = VoiceActor.where(id: favorite_voice_actor_ids)
   end
 
   def update
     FavoriteVoiceActor.create(user: current_user, voice_actor_id: params['favorite_voice_actor_id_1'])
     FavoriteVoiceActor.create(user: current_user, voice_actor_id: params['favorite_voice_actor_id_2'])
     FavoriteVoiceActor.create(user: current_user, voice_actor_id: params['favorite_voice_actor_id_3'])
-    redirect_to edit_favorite_voice_actors_path
+    redirect_to user_path(current_user)
   end
 end
