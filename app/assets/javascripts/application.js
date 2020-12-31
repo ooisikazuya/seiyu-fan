a61ffb1f5cfa298c11aa07ad21adc547a6d312cc
ooisikazuya <i1no1su6ke@gmail.com>
2020-12-31 14:50:37 +0900

    選択肢の表示・非表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,38 @@
 //= require_tree .
 //= require jquery 
 //= require jquery_ujs
-
-
+//$(window).on('load', function() {    $('select').on('click', function() {   $('select[name="favorite_voice_actor_id_1 favorite_voice_actor_id_2 favorite_voice_actor_id_3"] option[value=' + $("select").val() + ']').remove();
+//})
+//});
+//$(function() {
 $(function() {
-  $("select").change(function() {
-    if ($(this).attr('id') == 'favorite_voice_actor_id_1') {
-      $('#favorite_voice_actor_id_2 option[value=' + $(this).val() + ']').remove();
-      $('#favorite_voice_actor_id_3 option[value=' + $(this).val() + ']').remove();
-    } else if ($(this).attr('id') == 'favorite_voice_actor_id_2') {
-      $('#favorite_voice_actor_id_1 option[value=' + $(this).val() + ']').remove();
-      $('#favorite_voice_actor_id_3 option[value=' + $(this).val() + ']').remove();
-    } else if ($(this).attr('id') == 'favorite_voice_actor_id_3') {
-      $('#favorite_voice_actor_id_2 option[value=' + $(this).val() + ']').remove();
-      $('#favorite_voice_actor_id_1 option[value=' + $(this).val() + ']').remove();
-    }
-  });
+  $(document).on('focus', 'select', function() {
+    const actorName1 = $("#favorite_voice_actor_id_1").val();
+    const actorName2 = $("#favorite_voice_actor_id_2").val();
+    const actorName3 = $("#favorite_voice_actor_id_3").val();
+    $('#favorite_voice_actor_id_1 option[value=' + actorName2  + ']').hide();
+    $('#favorite_voice_actor_id_1 option[value=' + actorName3  + ']').hide();
+    $('#favorite_voice_actor_id_2 option[value=' + actorName1  + ']').hide();
+    $('#favorite_voice_actor_id_2 option[value=' + actorName3  + ']').hide();
+    $('#favorite_voice_actor_id_3 option[value=' + actorName1  + ']').hide();
+    $('#favorite_voice_actor_id_3 option[value=' + actorName2  + ']').hide();
+    $(document).on('change', 'select', function() {
+      if ($(this).attr('id') == 'favorite_voice_actor_id_1') {
+        $('#favorite_voice_actor_id_2 option[value=' + $(this).val() + ']').hide();
+        $('#favorite_voice_actor_id_2 option[value=' + actorName1 + ']').show();
+        $('#favorite_voice_actor_id_3 option[value=' + $(this).val() + ']').hide();
+        $('#favorite_voice_actor_id_3 option[value=' + actorName1 + ']').show();
+      } else if ($(this).attr('id') == 'favorite_voice_actor_id_2') {
+        $('#favorite_voice_actor_id_1 option[value=' + $(this).val() + ']').hide();
+        $('#favorite_voice_actor_id_1 option[value=' + actorName2 + ']').show();
+        $('#favorite_voice_actor_id_3 option[value=' + $(this).val() + ']').hide();
+        $('#favorite_voice_actor_id_3 option[value=' + actorName2 + ']').show();
+      } else if ($(this).attr('id') == 'favorite_voice_actor_id_3') {
+        $('#favorite_voice_actor_id_1 option[value=' + $(this).val() + ']').hide();
+        $('#favorite_voice_actor_id_1 option[value=' + actorName3 + ']').show();
+        $('#favorite_voice_actor_id_2 option[value=' + $(this).val() + ']').hide();
+        $('#favorite_voice_actor_id_2 option[value=' + actorName3 + ']').show();
+      }
+    });
+  }); 
 });
