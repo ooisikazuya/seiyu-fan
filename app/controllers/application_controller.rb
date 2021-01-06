deb6a91cb482935ee3ed05a9133dfcaa7094fc3e
ooisikazuya <i1no1su6ke@gmail.com>
2021-01-06 17:22:33 +0900

    ニックネーム変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     added_attrs = [:nickname]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
