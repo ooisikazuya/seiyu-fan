6d78f11fd96410abf9dd17d9d31e70d507eec6df
ooisikazuya <i1no1su6ke@gmail.com>
2021-06-10 17:42:26 +0900

    favorite_voice_actors_controllerの変更

--- app/controllers/favorite_voice_actors_controller.rb
+++ app/controllers/favorite_voice_actors_controller.rb
@@ -1,13 +1,13 @@
 class FavoriteVoiceActorsController < ApplicationController
+  before_action :set_user, only: [:index, :edit]
+
   def index
-    @user = current_user
     favorite_voice_actor_ids = @user.favorite_voice_actors.pluck(:voice_actor_id)
     @favorite_voice_actors = VoiceActor.where(id: favorite_voice_actor_ids)
   end
   
   def edit
     @voice_actors_array = VoiceActor.all.map { |actor| ["#{actor.last_name} #{actor.first_name}", actor.id] }
-    @user = current_user
     favorite_voice_actor_ids = @user.favorite_voice_actors.pluck(:voice_actor_id)
     @favorite_voice_actors = VoiceActor.where(id: favorite_voice_actor_ids)
   end
@@ -18,5 +18,11 @@ class FavoriteVoiceActorsController < ApplicationController
     FavoriteVoiceActor.create(user: current_user, voice_actor_id: params['favorite_voice_actor_id_2'])
     FavoriteVoiceActor.create(user: current_user, voice_actor_id: params['favorite_voice_actor_id_3'])
     redirect_to user_path(current_user)
+  end
+
+  private
+
+  def set_user
+    @user = current_user
   end
 end
